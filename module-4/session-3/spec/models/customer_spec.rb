3dd796e1d5a2c55d30f6bdea035ec85b09fc7efb
umar006 <umaralfaruq597@gmail.com>
2021-08-01 19:30:14 +0700

    success test #save method by given valid input

---
+++ module-4/session-3/spec/models/customer_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../../config/env/development'
+require_relative '../../app/models/customer'
+
+describe Customer do
+  before(:each) do
+    @customer_valid = Customer.new({
+                                     id: 99,
+                                     name: 'umar',
+                                     phone: '+6289689999'
+                                   })
+
+    @customer_invalid = Customer.new({
+                                       id: 99,
+                                       name: nil,
+                                       phone: nil
+                                     })
+
+    @dummy_database = double
+    allow(Mysql2::Client).to receive(:new).and_return(@dummy_database)
+  end
+
+  describe '#save' do
+    context 'given valid input' do
+      it 'should save to database' do
+        expect(@dummy_database).to receive(:query).with("
+            insert into customers (name, phone) values ('#{@customer_valid.name}', '#{@customer_valid.phone}');
+          ".gsub(/\s+/, ' '))
+        @customer_valid.save
+      end
+    end
+  end
+end
