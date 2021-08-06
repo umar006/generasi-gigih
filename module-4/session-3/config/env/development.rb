50bc8a26c67f94e573e30d0a6d82eaa730e751f8
umar006 <umaralfaruq597@gmail.com>
2021-08-06 14:29:07 +0700

    update minor

--- module-4/session-3/config/env/development.rb
+++ module-4/session-3/config/env/development.rb
@@ -1,12 +1,10 @@
 require 'mysql2'
 
 def create_db_client
-  client = Mysql2::Client.new(
-    :host => 'localhost',
-    :username => 'root',
-    :password => '',
-    :database => 'food_oms_db'
+  Mysql2::Client.new(
+    host: 'localhost',
+    username: 'root',
+    password: '',
+    database: 'food_oms_db'
   )
-
-  client
 end
